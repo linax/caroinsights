4dd81e76c90729e5ddd4087a591775131c8c4a3a
linax <c.lagosv@gmail.com>
2024-09-20 23:08:22 -0300

    fix siteheader

--- src/app/SiteHeader.tsx
+++ src/app/SiteHeader.tsx
@@ -1,20 +1,8 @@
 "use client";
-
-import React, { Fragment, useEffect, useMemo, useState } from "react";
-import { usePathname } from "next/navigation";
-import HeaderLogged from "@/components/Header/HeaderLogged";
-import Header from "@/components/Header/Header";
-import Header2 from "@/components/Header/Header2";
-import {
-  ShoppingBagIcon as ShoppingCartIcon,
-  Cog8ToothIcon as CogIcon,
-} from "@heroicons/react/24/outline";
-import { Popover, Transition } from "@headlessui/react";
-import SwitchDarkMode2 from "@/components/SwitchDarkMode/SwitchDarkMode2";
 import { useThemeMode } from "@/hooks/useThemeMode";
 
 const SiteHeader = () => {
-  let pathname = usePathname();
+
   useThemeMode();
   //
 
