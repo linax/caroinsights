f9c87ede7f473f6bf7d3b03a4b00e395877a8b13
linax <c.lagosv@gmail.com>
2025-02-27 23:09:26 -0300

    fixing video page

--- src/components/PostMeta2/PostMeta2.tsx
+++ src/components/PostMeta2/PostMeta2.tsx
@@ -1,51 +1,29 @@
-import React, { FC } from "react";
-import Avatar from "@/components/Avatar/Avatar";
-import { PostDataType } from "@/data/types";
-import { DEMO_POSTS } from "@/data/posts";
-import Link from "next/link";
+import React, { FC } from "react"
+import Avatar from "@/components/Avatar/Avatar"
+import { PostDataType } from "@/data/types"
+import { DEMO_POSTS } from "@/data/posts"
+import Link from "next/link"
 
-const metaDemo: PostMeta2Props["meta"] = DEMO_POSTS[0];
+const metaDemo: PostMeta2Props["meta"] = DEMO_POSTS[0]
 
 export interface PostMeta2Props {
-  className?: string;
-  meta?: Pick<PostDataType, "date" | "author" | "categories" | "readingTime">;
-  hiddenCategories?: boolean;
-  size?: "large" | "normal";
-  avatarRounded?: string;
+  className?: string
+  meta?: Pick<PostDataType, "date" | "author" | "categories" | "readingTime">
+  hiddenCategories?: boolean
+  size?: "large" | "normal"
+  avatarRounded?: string
 }
 
-const PostMeta2: FC<PostMeta2Props> = ({
-  className = "leading-none",
-  meta = metaDemo,
-  hiddenCategories = false,
-  size = "normal",
-  avatarRounded,
-}) => {
-  const { date, author, categories, readingTime } = meta;
+const PostMeta2: FC<PostMeta2Props> = ({ className = "leading-none", meta = metaDemo, hiddenCategories = false, size = "normal", avatarRounded }) => {
+  const { author, categories } = meta
   return (
-    <div
-      className={`nc-PostMeta2 flex items-center flex-wrap text-neutral-700 text-left dark:text-neutral-200 ${
-        size === "normal" ? "text-xs" : "text-sm"
-      } ${className}`}
-    >
-      <Link
-        href={author.href}
-        className="flex items-center space-x-2 rtl:space-x-reverse"
-      >
-        <Avatar
-          radius={avatarRounded}
-          sizeClass={
-            size === "normal"
-              ? "h-6 w-6 text-sm"
-              : "h-10 w-10 sm:h-11 sm:w-11 text-xl"
-          }
-          imgUrl={author.avatar}
-          userName={author.displayName}
-        />
+    <div className={`nc-PostMeta2 flex items-center flex-wrap text-neutral-700 text-left dark:text-neutral-200 ${size === "normal" ? "text-xs" : "text-sm"} ${className}`}>
+      <Link href={author.href} className="flex items-center space-x-2 rtl:space-x-reverse">
+        <Avatar radius={avatarRounded} sizeClass={size === "normal" ? "h-6 w-6 text-sm" : "h-10 w-10 sm:h-11 sm:w-11 text-xl"} imgUrl={author.avatar} userName={author.displayName} />
       </Link>
       <div className="ms-3">
         <div className="flex items-center">
-          <Link href={author.href} className="block font-semibold">
+          <Link href="#" className="block font-semibold">
             {author.displayName}
           </Link>
 
@@ -64,16 +42,10 @@ const PostMeta2: FC<PostMeta2Props> = ({
             </>
           )}
         </div>
-        <div className="text-xs mt-[6px]">
-          <span className="text-neutral-700 dark:text-neutral-300">{date}</span>
-          <span className="mx-2 font-semibold">·</span>
-          <span className="text-neutral-700 dark:text-neutral-300">
-            {readingTime} min read
-          </span>
-        </div>
+        <div className="text-xs mt-[6px]"></div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default PostMeta2;
+export default PostMeta2
